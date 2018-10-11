926d508a205410123b522a1788fdad1371f18694
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2018-10-11 12:13:59 -0300

    - Improve the script output

--- find-exec.sh
+++ find-exec.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+# variables of the colors
 cyan='\033[36;1m'
 red='\033[31;1m'
+green='\033[32;1m'
 end='\033[m'
 
 while [ $1 ]; do
-   if type "$1" &> /dev/null; then
-      echo -e "${cyan}${1} is installed in the system.${end}"
+   if which "$1" &> /dev/null; then
+      # get the location of the directory to show in the output
+      dir=$(which "$1")
+	echo -e "${cyan}${1} is installed in the system.${end} ${green}Directory: ${dir}${end}"
    else
       echo -e "${red}${1} is not installed in the system.${end}"
    fi
