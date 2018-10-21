b82eda23fcb23fdeecce961760b7ac035db732dd
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2018-10-20 21:48:36 -0300

    - some adjusts to redirect the output of the command

--- find-exec.sh
+++ find-exec.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 # variables of the colors
-cyan=$(tput bold; tput setaf 6)
-red=$(tput bold; tput setaf 1)
-green=$(tput bold; tput setaf 2)
+cyan_bold=$(tput bold; tput setaf 6)
+cyan=$(tput setaf 6)
+red_bold=$(tput bold; tput setaf 1)
+red=$(tput setaf 1)
 end=$(tput sgr0)
 
 while [ $1 ]; do
-   if type "$1" > /dev/null; then
+   if type "$1" >/dev/null 1>/dev/null 2>/dev/null; then
       # get the location of the directory to show in the output
-      echo "${cyan}${1} is installed in the system.${end}"
+      echo "${cyan_bold}${1}${end} ${cyan}is installed in the system.${end}"
       # Directory: ${dir}${end}
    else
-      echo "${red}${1} is not installed in the system.${end}"
+      echo "${red_bold}${1}${end} ${red}is not installed in the system.${end}"
    fi
    shift
 done
