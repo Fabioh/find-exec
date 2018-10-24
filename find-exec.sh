902788a1f02672c9f8e07f68c888b8643495c2f6
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2018-10-24 18:39:07 +0000

    -fix issues in script

--- find-exec.sh
+++ find-exec.sh
@@ -8,7 +8,7 @@ red=$(tput setaf 1)
 end=$(tput sgr0)
 
 while [ $1 ]; do
-   if type "$1" >/dev/null 1>/dev/null 2>/dev/null; then
+   if type "$1" 1>/dev/null 2>/dev/null; then
       # get the location of the directory to show in the output
       echo "${cyan_bold}${1}${end} ${cyan}is installed in the system.${end}"
       # Directory: ${dir}${end}
